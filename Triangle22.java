017a07e091e65f882084bf9fd55e6cb6c599b184
rizdannn <rafifprastana@gmail.com>
2025-11-02 20:45:44 +0700

    Percobaan 3

---
+++ Triangle22.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Triangle22 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner (System.in); 
+        
+       System.out.print("Masukkan nilai N = ");
+       int n = sc.nextInt();
+       int i = 0;
+
+       while(i <= n) {
+        int j = 0;
+        while(j < i) {
+            System.out.print(" * ");
+            j++;
+        }
+        System.out.println("");
+        i++;
+       }
+    
+
+    }
+}
