7ccee4d457d9dab1c0d3959f52e1a54d3f11b323
rizdannn <rafifprastana@gmail.com>
2025-11-03 15:09:03 +0700

    Percobaan 4

---
+++ PenilaianKelompok22.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PenilaianKelompok22 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1;
+        double nilai, totalNilai, rataNilai;
+
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+
+            totalNilai = 0;
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(" Nilai dari Kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+
+            rataNilai = totalNilai / 5;
+            System.out.println("Rata-rata nilai kelompok " + i + ": " + rataNilai);
+            System.out.println();
+
+            i++;
+        }
+
+    }
+}
